e4fc32fac99a3d75a0ba52270763812391a1c0bc
tanghonbgxu <914076301@qq.com>
2019-02-12 11:47:39 +0800

    init

--- src/Assembler.php
+++ src/Assembler.php
@@ -4,11 +4,62 @@ namespace LaravelAssembler;
 
 class Assembler
 {
+    /**
+     * @var string
+     */
+    protected $fields;
+
+    /**
+     * @var Object
+     */
     protected $dto;
+
+    /**
+     * @var Object
+     */
+    protected $assembler;
+
+    /**
+     * Assembler constructor.
+     *
+     * @param $dto
+     *
+     * @throws \Exception
+     */
+    public function __construct($dto)
+    {
+        $config = config('assembler');
+
+        if (empty($config['query_name'])) {
+            throw new \Exception('Invalid assembler config "query_name"');
+        }
+
+        $this->fields = request()->get($config['query_name']);
+
+        $dtoClassName = get_class($dto);
+
+        if (isset($config['maps'][$dtoClassName])) {
+            $this->setAssembler(new $config['maps'][$dtoClassName]);
+        } else {
+            $this->assembler = $dto;
+        }
+
+        $this->dto = $dto;
+    }
+
+    public function setFields(string $fields)
+    {
+        $this->fields = $fields;
+    }
+
+    public function setAssembler($assembler)
+    {
+        $this->assembler = $assembler;
+    }
 
     public function __get($name)
     {
-        // TODO: Implement __get() method.
+        return $this->assembler->$name;
     }
 
 }
