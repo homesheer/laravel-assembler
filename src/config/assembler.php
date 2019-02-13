72b738fba21378c78e199821843b5598ca7232ba
tanghonbgxu <914076301@qq.com>
2019-02-13 11:28:50 +0800

    add getter name prefix config

--- src/config/assembler.php
+++ src/config/assembler.php
@@ -18,9 +18,9 @@ return [
     | assembler类中获取字段数据的方法名前缀
     |--------------------------------------------------------------------------
     |
-    | 通过下面配置的查询字符串名指定要获取的字段，如指定字符串名为fields，
-    | 要获取user上的id，name和值对象address的province, city字段，url格式如下：
-    | http://localhots/user?fields={id,name,address{province,city}}
+    | 如指定要获取User模型上的id和name字段，则可以在Assembler上定义方法getId()和getName()，
+    | 这些方法的get前缀就是在此处配置。
+    | 获取字段数据的顺序：Assembler的getter方法 -> Model/Dto的getter方法 -> Model/Dto的属性
     */
 
     'getter_name_prefix' => 'get',
